d1f9d866be0a9bcd9d205461f3696ded903e6c98
Master <Master@DESKTOP-K6NGF27>
2022-09-17 20:59:13 +0900

    finished

--- WordMasterProject2/src/com/dong/word/WordManager.java
+++ WordMasterProject2/src/com/dong/word/WordManager.java
@@ -55,6 +55,12 @@ public class WordManager {
 			else if(menu == 1) {
 				wordCRUD.listAll();
 			}
+			else if(menu == 2) {
+				wordCRUD.searchLevel();
+			}
+			else if(menu == 3) {
+				wordCRUD.searchWord();
+			}
 			else if(menu == 5) {
 				wordCRUD.updateItem();
 			}
